3dc1165ef39a7ab229459a1e25ffb9ee63620d21
Denwood <KD.KouznetsovDenis@gmail.com>
2018-04-26 16:08:55 +0200

    After Installation
    After installation by script

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
-#!/bin/bash 
-
-#https://material.angular.io/guide/getting-started
+#!/bin/bash
+npm install -g @angular/cli
+cd $PWD
+ng new angular5-mat-clean
+cd angular5-mat-clean
 npm install --save @angular/material @angular/cdk
 npm install --save angular/material2-builds angular/cdk-builds
 npm install --save @angular/animations
 npm install --save hammerjs
 
+#start :
+# ng serve 
+#start with specific port : 
+# ng serve --port 4500
 
 exit 0 
