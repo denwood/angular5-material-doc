b9f9d01e7f92c509a034c6d653036c3f0809aa35
Denwood <KD.KouznetsovDenis@gmail.com>
2018-04-26 15:57:09 +0200

    Installation Script for Linux

---
+++ material.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#https://material.angular.io/guide/getting-started
+npm install --save @angular/material @angular/cdk
+npm install --save angular/material2-builds angular/cdk-builds
+npm install --save @angular/animations
+npm install --save hammerjs
+
+
+exit 0 
